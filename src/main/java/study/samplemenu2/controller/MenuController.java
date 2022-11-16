5454363a6e485f84299cc0651d999f10c54d467d
leesangyeop <infightersy@naver.com>
2022-11-16 14:59:06 +0900

    화면 그리는 중

---
+++ src/main/java/study/samplemenu2/controller/MenuController.java
@@ -0,0 +1,26 @@
+package study.samplemenu2.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import study.samplemenu2.service.MenuService;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class MenuController {
+
+    private final MenuService menuService;
+
+    @GetMapping("/menu/list")
+    public String menuList(Model model) {
+        List<Map<String, Object>> list = menuService.findList();
+        model.addAttribute("list", list);
+        return "menu/menuList";
+    }
+}
